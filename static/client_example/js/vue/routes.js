eeccf751cca43af5ba2ae831569c153ecfa48dee
aaaasmile <testhaus@gmx.at>
2021-01-06 23:42:09 +0100

    separate client and server vue app

---
+++ static/client_example/js/vue/routes.js
@@ -0,0 +1,5 @@
+import CommentView from './views/commentlview.js'
+
+export default [
+  { path: '/', icon: 'dashboard', title: 'CommentView', component: CommentView },
+]
---
+++ static/js-server/vue/views/commentlview.vue
@@ -0,0 +1,7 @@
+<template>
+  <v-row justify="center">
+    <v-col xs="12" sm="12" md="10" lg="8" xl="6">
+      Comments on Server
+    </v-col>
+  </v-row>
+</template>
