88c6f002ffe38469dcccb4025f7018dcba99a737
aaaasmile <testhaus@gmx.at>
2021-01-07 22:05:20 +0100

    update with divider

--- static/client_example/js/vue/store/comment-store.js
+++ static/client_example/js/vue/store/comment-store.js
@@ -5,6 +5,7 @@ export default {
         tot_comments: 1,
         comments: [
             { content: 'This is a blog comment 1', user: 'Gianni', avatar: 'https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50', post_id: 42, parent_id: 0, id: 20 },
+            { divider: true, inset: true },
             { content: 'This is a blog comment 2', user: 'Luzzo', avatar: 'https://www.gravatar.com/avatar/233330b479e2e5b48aec07710c08d50/?d=mp', post_id: 42, parent_id: 0, id: 19 },
             { content: 'This is a Reply blog comment 1-2', user: 'Bruno', post_id: 42, parent_id: 20, id: 21 },
         ],
--- static/client_example/js/vue/views/commentlview.vue
+++ static/client_example/js/vue/views/commentlview.vue
@@ -1,17 +1,32 @@
 <template>
   <v-card class="mx-auto">
-    <v-card-title>{{NumComment}} {{TitleCommenti}}</v-card-title>
+    <v-card-title>{{ NumComment }} {{ TitleCommenti }}</v-card-title>
     <v-container>
-      <v-list two-line>
-        <v-list-item v-for="item in Comments" :key="item.id">
-          <v-list-item-avatar>
-            <v-img :src="item.avatar"></v-img>
-          </v-list-item-avatar>
-          <v-list-item-content> 
-            <v-list-item-title v-html="item.user"></v-list-item-title>
-            <v-list-item-subtitle v-html="item.content"></v-list-item-subtitle>
-          </v-list-item-content>
-        </v-list-item>
+      <v-list three-line>
+        <template v-for="(item, index) in Comments">
+          <v-divider
+            v-if="item.divider"
+            :key="index"
+            :inset="item.inset"
+          ></v-divider>
+          <v-list-item v-else :key="item.id">
+            <v-list-item-avatar>
+              <v-img :src="item.avatar"></v-img>
+            </v-list-item-avatar>
+            <v-list-item-content>
+              <v-list-item-title v-html="item.user"></v-list-item-title>
+              <v-list-item-subtitle
+                v-html="item.content"
+              ></v-list-item-subtitle>
+              <v-expansion-panels :flat="true">
+                <v-expansion-panel>
+                  <v-expansion-panel-header>1 Risposta</v-expansion-panel-header>
+                  <v-expansion-panel-content> </v-expansion-panel-content>
+                </v-expansion-panel>
+              </v-expansion-panels>
+            </v-list-item-content>
+          </v-list-item>
+        </template>
       </v-list>
     </v-container>
   </v-card>
